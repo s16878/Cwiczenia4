ea247e3cd99c704fa1965c39ad209338d7816296
s16878 <61700945+s16878@users.noreply.github.com>
2020-03-21 23:14:23 +0100

    Add files via upload

---
+++ MockDbServices.cs
@@ -0,0 +1,25 @@
+﻿using Cw3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cw3.Controllers.Services
+{
+    public class MockDbServices : IdbServices
+    {
+        public static IEnumerable<Student> _students = new List<Student>
+        {
+            new Student{IdStudent=1, FirstName="Jan", LastName="Kowalski", IndexNumber="s1234"},
+            new Student{IdStudent=2, FirstName="Jan", LastName="Nowak", IndexNumber="s567"},
+            new Student{IdStudent=3, FirstName="Jan", LastName="XXX", IndexNumber="s3455"}
+        };
+
+        public IEnumerable<Student> GetStudents()
+        {
+            return _students;
+        }
+
+        
+    }
+}
